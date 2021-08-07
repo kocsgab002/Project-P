8eedb1945f8e0452c417ea071163458783871452
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2021-08-07 12:04:50 +0200

    javítások

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const weather_url = 'https://api.openweathermap.org/data/2.5/weather?q=Budapest&
 async function getWeather() {
     const respone = await fetch(weather_url);
     const data = await respone.json();
-    let homerseklet =document.getElementById('temp');
-    homerseklet.innerHTML += (Math.round(data.main.temp*10)/10);
-    console.log(data.main.temp);
+    let homerseklet = document.getElementById('temp');
+    homerseklet.innerHTML += (Math.round(data.main.temp * 10) / 10);
+    console.log("Aktuális hőmérséklet:",(data.main.temp));
 };
 getWeather();
 
