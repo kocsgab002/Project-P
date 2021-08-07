c86d2e7eec9b18bf0c4b5e9a1445563bacd6d4f2
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2021-08-07 21:31:33 +0200

    js javítás

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 const myapi = 'a3a6e8f3dd3bbd3debaa600a7e2ac7ce'; //saját API ID //
-const weather_url = 'https://api.openweathermap.org/data/2.5/weather?q=Szerencs&units=metric&lang=hu&appid=a3a6e8f3dd3bbd3debaa600a7e2ac7ce'; //API URL //
+const weather_url = 'https://api.openweathermap.org/data/2.5/weather?q=Budapest&units=metric&lang=hu&appid=a3a6e8f3dd3bbd3debaa600a7e2ac7ce'; //API URL //
 
 //Elkészítem a 'getWeather' függvényt //
 async function getWeather() {
@@ -20,12 +20,37 @@ async function getWeather() {
     let clouds = document.getElementById('clouds');
     let szoveg = data.weather[0].description; //Kiválasztom a tömb 0. sorának 'description' elemét //
     let szoveg_nagybetus = szoveg.charAt(0).toUpperCase() + szoveg.slice(1); // Szöveg eleje nagybetű // 
-    clouds.innerHTML += szoveg_nagybetus; //
+    clouds.innerHTML += szoveg_nagybetus; //Szöveges időjárás kiírása //
+
+    //Min.Hőm. kiiratása//
+    let mintemp = document.getElementById('mintemp');
+    mintemp.innerHTML += (Math.round(data.main.temp_min * 10) / 10);
+
+    //Max.Hőm. kiiratása//
+    let maxtemp = document.getElementById('maxtemp');
+    maxtemp.innerHTML += (Math.round(data.main.temp_max * 10) / 10);
+
+    //Páratartalom kiiratása//
+    let humidity = document.getElementById('humidity');
+    humidity.innerHTML += data.main.humidity;
+
+    //Légnyomás kiiratása//
+    let pressure = document.getElementById('pressure');
+    pressure.innerHTML += data.main.pressure;
+
+    //Napkelte kiitarása//
+    let sec = data.sys.sunrise;
+    let date = new Date(sec * 1000);
+    let timestr = date.toLocaleTimeString();
+    let sunrise = document.getElementById('sunrise');
+    sunrise.innerHTML += timestr;
 
 
 };
 
 //Függvény meghívása //
 getWeather();
+
+
 
 
