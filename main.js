c9db2166df391a99a97a14744276f82af4aeaab6
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2021-08-07 11:22:16 +0200

    első JS probalkozas

---
+++ main.js
@@ -0,0 +1,12 @@
+const myapi = 'a3a6e8f3dd3bbd3debaa600a7e2ac7ce';
+const weather_url = 'https://api.openweathermap.org/data/2.5/weather?q=Budapest&units=metric&lang=hu&appid=a3a6e8f3dd3bbd3debaa600a7e2ac7ce';
+async function getWeather() {
+    const respone = await fetch(weather_url);
+    const data = await respone.json();
+    let homerseklet =document.getElementById('temp');
+    homerseklet.innerHTML += (Math.round(data.main.temp*10)/10);
+    console.log(data.main.temp);
+};
+getWeather();
+
+
